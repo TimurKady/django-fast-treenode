81eabd86cffda25f4364343bd385f092c8a27286
Timur Kady <kadyevtr@gmail.com>
2023-06-01 23:18:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-fast-treenode',
-      version='1.0',
+      version='1.0.2',
       description='Combination of Adjacency List and Closure Table for supporting tree (hierarchical) data structure in Django projects.',
       packages=['django-fast-treenode-1.0.tar'],
       author_email='timurkady@yandex.com',
