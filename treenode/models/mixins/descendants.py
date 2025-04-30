a731a6c7a25253b608d4c276c0d9a1dadfcb654f
Timur Kady <kadyevtr@gmail.com>
2025-04-30 14:51:06 +0300

    [3.0.2]
    Security Update and SQL Optimization

--- treenode/models/mixins/descendants.py
+++ treenode/models/mixins/descendants.py
@@ -32,19 +32,23 @@ class TreeNodeDescendantsMixin(models.Model):
     def get_descendants_queryset(self, include_self=False, depth=None):
         """Get the descendants queryset."""
         path = self.get_order()  # calls refresh and gets the current _path
-        from_path = path + '.'
+
+        # from_path = path + '.'
         # to_path = path + '/'
 
         # options = {'_path__gte': from_path, '_path__lt': to_path}
         # if depth:
         #    options["_depth__lt"] = depth
         # queryset = self._meta.model.objects.filter(**options)
-        queryset = self._meta.model.objects.filter(_path__startswith=from_path)
+        # if include_self:
+        #     return self._meta.model.objects.filter(pk=self.pk) | queryset
+        # else:
+        #    return queryset
 
-        if include_self:
-            return self._meta.model.objects.filter(pk=self.pk) | queryset
-        else:
-            return queryset
+        suffix = "" if include_self else '.'
+        path += suffix
+        queryset = self._meta.model.objects.filter(_path__startswith=path)
+        return queryset
 
     @cached_method
     def get_descendants_pks(self, include_self=False, depth=None):
