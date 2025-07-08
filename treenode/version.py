7c715024f92ed38c48631125c64b734978364273
Timur Kady <kadyevtr@gmail.com>
2025-07-08 15:58:49 +0300

    [3.2.1]

--- treenode/version.py
+++ treenode/version.py
@@ -4,9 +4,9 @@ TreeNode Version Module
 
 This module defines the current version of the TreeNode package.
 
-Version: 3.2.0
+Version: 3.2.1
 Author: Timur Kady
 Email: timurkady@yandex.com
 """
 
-__version__ = '3.2.0'
+__version__ = '3.2.1'
